cb15a5de4dd7aeebc52391d9274057017297ae56
Mystievous <artemis@fineonline.com>
2022-12-16 19:48:40 -0800

    spirit voice lines

--- assets/mctc/sounds/steve/convert.sh
+++ assets/mctc/sounds/steve/convert.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 for f in ./*.ogg; do
-  ffmpeg -i "$f" -filter_complex '[0:a]channelsplit=channel_layout=stereo:channels=FL[left]' -map '[left]' "${f%.ogg}.mono.ogg"
+  ffmpeg -i "$f" "${f%.wav}.ogg"
 done
